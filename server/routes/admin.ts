1d6061df8e75e572a8abd370ccd1d54d4c195003
Krzysztof Marecki <marecki.krzysztof@gmail.com>
2016-07-11 23:53:41 +0200

    Add bootstrap

--- server/routes/admin.ts
+++ server/routes/admin.ts
@@ -3,5 +3,5 @@ import * as express from 'express';
 export let router = express.Router();
 
 router.get('/admin', (req: express.Request, res: express.Response) => {
-    res.render('admin/index', { });
+    res.render('admin/index', { model: {title: 'Admin page'} });
 });
--- server/routes/app.ts
+++ server/routes/app.ts
@@ -3,5 +3,5 @@ import * as express from 'express';
 export let router = express.Router();
 
 router.get('/', (req: express.Request, res: express.Response) => {
-    res.render('index', { });
+    res.render('index', { model: {title: 'Blog page'} });
 });
