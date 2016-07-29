bdaa44e365b703e9c6cdeebec937ff7d694c9af8
Krzysztof Marecki <marecki.krzysztof@gmail.com>
2016-07-29 22:38:11 +0200

    Serve index.html for ng-view paths

--- server/app.ts
+++ server/app.ts
@@ -3,20 +3,24 @@ import * as express from 'express';
 import * as http from 'http';
 import * as path from 'path';
 
+import {ExpressApp} from 'express-app';
 import {MongoConfiguration} from 'mongoose-repos';
+import * as adminRoutes from './routes/admin';
 import * as apiRoutes from './routes/api';
 
 let config = require('config');
 let root =  path.join(__dirname, '..');
+let publicPath = path.join(root, 'app');
 let app = express();
+ExpressApp.physicalPath = publicPath;
 
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(root, 'views'));
 app.set('view engine', 'jade');
 
-let publicPath = path.join(root, 'app');
-app.use('/', express.static(publicPath));
+app.use(adminRoutes.router);
 app.use(apiRoutes.router);
+app.use('/', express.static(ExpressApp.physicalPath));
 
 MongoConfiguration.uri = config.MongoDb.uri;
 
---
+++ server/routes/admin.ts
@@ -0,0 +1,21 @@
+import * as express from 'express';
+import * as fs from 'fs';
+import * as path from 'path';
+
+import {ExpressApp} from 'express-app';
+
+export let router = express.Router();
+
+router.get('/admin/*', (req: express.Request, res: express.Response) => {
+    console.log(req.path);
+    let filePath = path.join(ExpressApp.physicalPath, req.path);
+    fs.exists(filePath, (exists) => {
+        if (exists) {
+            res.sendFile(filePath);
+        } else {
+            let indexPath = path.join(ExpressApp.physicalPath, 'admin', 'index.html');
+            res.sendFile(indexPath);
+        }
+    });
+});
+
