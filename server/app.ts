1f5d5520383ba52fa262644f9fa5a700b427296d
Krzysztof M <marecki.krzysztof@gmail.com>
2016-10-20 23:24:33 +0200

    Fix routing

--- server/app.ts
+++ server/app.ts
@@ -6,26 +6,26 @@ import * as path from 'path';
 import {ExpressApp} from 'express-app';
 import {MongoConfiguration, MongoDb} from 'mongoose-repos';
 
+import * as adminRoutes from './routes/admin';
+import * as apiRoutes from './routes/api';
+
 let config = require('config');
 let root =  path.join(__dirname, '..');
+let publicPath = path.join(root, 'app');
 let app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-ExpressApp.physicalPath = path.join(root, 'app');
+ExpressApp.physicalPath = publicPath;
 
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(root, 'views'));
 app.set('view engine', 'jade');
 
-import * as publicRoutes from './routes/public';
-import * as adminRoutes from './routes/admin';
-import * as apiRoutes from './routes/api';
-
-app.use(apiRoutes.router);
 app.use(adminRoutes.router);
-app.use(publicRoutes.router);
+app.use(apiRoutes.router);
+app.use('/', express.static(ExpressApp.physicalPath));
 
 MongoConfiguration.uri = config.MongoDb.uri;
 MongoConfiguration.useAutoIncrement = true;
