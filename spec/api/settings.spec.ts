36eb5281406613721356db6c5055339a85a33506
Krzysztof M <marecki.krzysztof@gmail.com>
2017-03-17 01:11:13 +0100

    Add Mongoclient test helper

--- spec/api/settings.spec.ts
+++ spec/api/settings.spec.ts
@@ -1,31 +1,36 @@
 process.env['NODE_ENV'] = 'test';
 process.env['NODE_CONFIG_DIR'] = '../config';
 
-import * as chai from 'chai';
-let chaiHttp = require('chai-http');
-let expect = chai.expect;
-// let should = chai.should();
+import { MongoHelper } from '../helpers/mongo';
 
 import server = require('../../server/server');
 import { IAliveResponse } from '../../shared/contracts/alive';
 import { SettingsRepository } from '../../server/db/settings';
 import { ISettings } from '../../shared/entities/settings';
 
+import * as chai from 'chai';
+let chaiHttp = require('chai-http');
+let expect = chai.expect;
+// let should = chai.should();
+
+let config = require('config');
+
 chai.use(chaiHttp);
 
 describe('settings', () => {
-    const db = new SettingsRepository();
+    const db = new MongoHelper();
     const settings: ISettings = {
         blogDescription: "Some description",
         blogName: "The Blog"
     }
 
     beforeEach((done) => {
-        db.saveSettings(settings, (err) => done())
+        db.open(config.MongoDb.uri)
+            .then(() => db.dropAll())
+            .then(() => db.insert('settings', settings))
+            .then(() => done());
     });
-    afterEach((done) => {
-        db.removeSettings((err) => done())
-    });
+    //afterEach((done) => done());
 
     it('/GET settings', (done) => {
         chai.request(server.app)
---
+++ spec/helpers/mongo.ts
@@ -0,0 +1,28 @@
+import * as _ from 'lodash';
+
+import { MongoClient, Db, InsertOneWriteOpResult } from 'mongodb';
+
+export class MongoHelper {
+    private db: Db;
+    open(uri: string): Promise<any> {
+        return MongoClient.connect(uri)
+            .then((db) => {
+                this.db = db;
+            })
+    }
+
+    insert(collectionName: string, docs: any): Promise<InsertOneWriteOpResult> {
+        return this.db.collection(collectionName).insert(docs);
+    } 
+    drop(collectionName: string): Promise<any> {
+        return this.db.collection(collectionName).drop()
+                .catch((err) => {} );
+    }
+
+    dropAll(): Promise<any> {
+        return this.db.collections()
+            .then((collections) => Promise.all(
+               _.each(collections, (collection) => collection.drop())));
+        
+    }
+}
