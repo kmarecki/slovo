d718857c392fb03f04bfe14f3235c452442cdff3
Krzysztof Marecki <marecki.krzysztof@gmail.com>
2017-04-06 19:12:47 +0200

    Add admin kara conf file

---
+++ karma.admin.conf.js
@@ -0,0 +1,62 @@
+function served(pattern) { return { pattern: pattern, included: false, served: true } };
+function included(pattern) { return { pattern: pattern, included: true, served: true } };
+
+module.exports = function (config) {
+  config.set({
+
+
+    basePath: './',
+
+    frameworks: ['systemjs', 'mocha'],
+
+    browsers: [
+      'ChromeDebugging'
+    ],
+
+    customLaunchers: {
+      ChromeDebugging: {
+        base: 'Chrome',
+        flags: ['--remote-debugging-port=9333']
+      }
+    },
+
+    files: [
+
+      //included('bower_components/angular/angular.js'),
+      //'bower_components/angular/angular.js',
+      served('app/**/*.js'),
+      served('app/**/*.html'),
+      'app/admin/components/**/*.spec.js'
+      //'components/articles-nav/articles-nav.spec.js'
+
+      //served('components/**/*.js')
+      // included('test-config.js')
+    ],
+
+    // proxies: {
+    //   '/bower_components/': '/base/bower_components/'
+    // },
+
+    autoWatch: true,
+
+    plugins: [
+      'karma-systemjs',
+      'karma-chrome-launcher',
+      'karma-firefox-launcher',
+      'karma-mocha'
+    ],
+
+    systemjs: {
+      configFile: 'app/system.karma.conf.js',
+      serveFiles: [
+        'app/**/*.js'
+      ],
+      config: {
+        paths: {
+
+          'angular-mocks': 'app/bower_components/angular-mocks/angular-mocks.js',
+        }
+      }
+    }
+  });
+};
